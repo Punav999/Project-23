35bd42ef47b9f0f524c6e4028cf4278a292ae0c6
Punav999 <71140824+Punav999@users.noreply.github.com>
2021-02-21 18:16:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,77 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.4, isStatic:true});
+	World.add(world, packageBody);
+	
+
+	//Create a Ground
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+	 World.add(world, ground);
+	 
+	 ground1 = Bodies.rectangle(600,600,20,120, {isStatic:true} );
+	 World.add(world, ground1);
+	 
+	 ground2 = Bodies.rectangle(720,600, 20,120 , {isStatic:true} );
+	 World.add(world, ground2);
+	 
+	 ground3 = Bodies.rectangle( 660,660,120,20, {isStatic:true} );
+ 	 World.add(world, ground3);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  rect(280,600,20,120);
+  rect(440,600,20,120);
+  rect(350,660, 160,20);
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+ if (keyCode === DOWN_ARROW) {
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+     Matter.Body.setStatic(packageBody,false)
+    
+  }
+}
+
+
+
